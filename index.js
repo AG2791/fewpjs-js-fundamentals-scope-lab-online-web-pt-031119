8f81d2e39ba8f5d7cf1edac5676b02ab7afd64fe
AJ Guiles <ashiguiles@gmail.com>
2019-10-19 03:09:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,28 @@ function add2(n) {
   // Feel free to move things around!
   two = 2
 }
+
+
+
+
+Learn more or give us feedback
+const animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  let animal = 'cat'
+
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+
+  return n + two
+}
